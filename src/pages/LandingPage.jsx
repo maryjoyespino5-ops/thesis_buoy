aed3f9d0b598a78d0220870b96b9d08339c76d83
maryjoyespino5-ops <villacampamichael123@gmail.com>
2026-08-02 23:58:01 +0800

    Make footer logo and name images bigger in LandingPage
    - logow.png: h-7 w-7 → h-10 w-10
    - name.png: h-5 → h-7
    - Footer branding now more prominent and readable

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1608,12 +1608,12 @@ function Footer() {
             <img
               src="/image/logow.png"
               alt="NELEUS1 logo"
-              className="h-7 w-7 object-contain"
+              className="h-10 w-10 object-contain"
             />
             <img
               src="/image/name.png"
               alt="NELEUS1"
-              className="h-5 w-auto object-contain"
+              className="h-7 w-auto object-contain"
             />
           </div>
           <p
